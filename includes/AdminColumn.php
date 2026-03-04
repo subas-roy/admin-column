d68bb8d2e544e5558270e1c6035aea6b4ed7bf43
subas-roy <subasroy46@gmail.com>
2026-03-04 17:41:43 +0600

    define constents

--- includes/AdminColumn.php
+++ includes/AdminColumn.php
@@ -11,7 +11,11 @@ class AdminColumn {
         add_action('plugins_loaded', [$this, 'init_plugin']);
     }
 
-    public function define_constents() {}
+    public function define_constents() {
+        define('ADMIN_COLUMN_VERSION', '1.0.0');
+        define('ADMIN_COLUMN_PLUGIN_DIR', plugin_dir_path(__DIR__)); // Needs to include files, so we need the directory path.
+        define('ADMIN_COLUMN_PLUGIN_URL', plugin_dir_url(__DIR__)); // Needs to load assets like CSS and JS, so we need the URL.
+    }
 
     public function init_plugin() {}
 
