916ea1e058630789478aaa4b43ef586cff1270c8
subas-roy <subasroy46@gmail.com>
2026-03-24 12:07:54 +0600

    added dependency check for required plugin

--- admin-column.php
+++ admin-column.php
@@ -10,6 +10,7 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       admin-column
+ * Requires Plugins: advanced-custom-fields
  */
 
 if (!defined('ABSPATH')) {
